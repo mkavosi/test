e52f2acdf710894046eee27783c76d4442d1b68d
mohammad kavosi <kavosi1010@gmail.com>
2018-01-02 18:20:32 +0330

    delete index.php

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
 <?php
-
-echo 'salam';
-$_SESSION['name']='abas hossio';
-echo 'what?';
-echo "Mother fuck";
-
-echo 'salam';
-$_SESSION['name']='abas hossio';
-echo 'what?';
-echo "Mother fuck";
-
-echo 'salam';
-$_SESSION['name']='abas hossio';
-echo 'what?';
-echo "Mother fuck";
